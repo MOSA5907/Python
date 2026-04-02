6c2227a58552f37d752348bc577fc5beb7b685c2
Moses Adjaye <moses@innobiz.co.za>
2026-04-02 21:11:11 +0000

    Updated Lesson 2

--- Lesson2/1.py
+++ Lesson2/1.py
@@ -3,26 +3,50 @@ import json
 with open("./Lesson2/data.json", "r") as f:
     data = json.load(f) 
 
-data["person"].append(
-    {
-        "name": "Moses",
-        "age": 66,
-        "city": "Accra"
-    }
-)
+# Print all keys in the data
+print("Print all keys in the data")
+print(data)
+for key, list in data.items():
+    print(f"{key}: {list}")
+    
+    for key1, value1 in list.items():
+        print(f"{key1}: {value1}")
 
-# data["person"][0] = {
-#             "name": "Moses",
-#             "age": 66,
-#             "city": "Accra"
-#         }
+print("------------------------------")
+   
+# Print all values in the data 
+print("Print all values in the data")
+print(data["person"][0])
+for key, value in data["person"][0].items():
+    print(f"{key}: {value}")
 
-with open("./Lesson2/new_data.json", "w") as f:
-    json.dump(data, f, indent=4)
+print("------------------------------")
+   
+# Print all names, ages, and cities in the data
+print("Print all names, ages, and cities in the data")
+print(data["person"])
+for i in data["person"]:
+    print(i["name"])
+    print(i["age"])
+    print(i["city"])
 
-with open("./Lesson2/new_data.json", "r") as f:
-    data1 = json.load(f)
+# for name, age, city in data["person"]:
+#     print(f"{name}: {age}, {city}")
 
-print(data1) 
+    # data["person"].append(
+    #     {
+    #         "name": name,
+    #         "age": age,
+    #         "city": city
+    #     }
+    # )
+
+# with open("./Lesson2/new_data.json", "w") as f:
+#     json.dump(data, f, indent=4)
+
+# with open("./Lesson2/new_data.json", "r") as f:
+#     data = json.load(f)
+
+# print(data) 
 # C:\website\Python\Lesson2\data.json
 # Lesson2\data.json
